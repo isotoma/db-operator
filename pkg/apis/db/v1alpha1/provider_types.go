5fddf7fa39f0d2bc53c22176b227807b5079f450
Doug Winter <doug.winter@isotoma.com>
2018-12-28 08:50:21 +0000

    added provider types

--- pkg/apis/db/v1alpha1/provider_types.go
+++ pkg/apis/db/v1alpha1/provider_types.go
@@ -11,6 +11,10 @@ import (
 type ProviderSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	Name    string   `json:"name"`
+	Image   string   `json:"image,omitempty"`
+	Command []string `json:"command,omitempty"`
+	Args    []string `json:"args,omitempty"`
 }
 
 // ProviderStatus defines the observed state of Provider
