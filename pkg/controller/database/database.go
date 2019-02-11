d0b6c2f6d8485b10c2e251ecf09057b9081c2a52
Doug Winter <doug.winter@isotoma.com>
2019-02-11 11:16:16 +0000

    slightly clearer where to put the code

--- pkg/controller/database/database.go
+++ pkg/controller/database/database.go
@@ -5,13 +5,19 @@ import dbv1alpha1 "github.com/isotoma/db-operator/pkg/apis/db/v1alpha1"
 func (r *ReconcileDatabase) Create(instance *dbv1alpha1.Database) chan error {
 	c := make(chan error)
 	go func() {
+		// Create a Job and wait for it
 		c <- nil
 	}()
 	return c
 }
 
 func (r *ReconcileDatabase) Drop(instance *dbv1alpha1.Database) chan error {
-
+	c := make(chan error)
+	go func() {
+		// Create a Job and wait for it
+		c <- nil
+	}()
+	return c
 }
 
 func (r *ReconcileDatabase) BackupThenDrop(instance *dbv1alpha1.Database) chan error {
@@ -19,7 +25,12 @@ func (r *ReconcileDatabase) BackupThenDrop(instance *dbv1alpha1.Database) chan e
 	go func() {
 		c2 := r.Backup(instance)
 		err := <-c2
-		c <- err
+		if err != nil {
+			c <- err
+		} else {
+			// call Drop
+			c <- nil
+		}
 	}()
 	return c
 }
